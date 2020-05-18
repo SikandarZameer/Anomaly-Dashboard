37a052f3e7cfe9b1c1a551f073e4f84e436f03bc
SikandarZameer <46181276+SikandarZameer@users.noreply.github.com>
2020-05-18 14:48:39 +0500

    line-charts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import BarGrouped from "./Components/Charts/BarGrouped";
+import LineCust from "./Components/Charts/LineCust";
+import Linestacked from "./Components/Charts/Linestacked";
+
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <BarGrouped />
+      <LineCust />
+      <Linestacked />
     </div>
   );
 }
