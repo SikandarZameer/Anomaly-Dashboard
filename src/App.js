13a0d86ec197d6997da58f29d30650183f090a89
SikandarZameer <46181276+SikandarZameer@users.noreply.github.com>
2020-05-18 19:19:44 +0500

    charts-typography

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from "react";
 // import LineCust from "./Components/Charts/LineCust";
 // import Linestacked from "./Components/Charts/Linestacked";
 // import DChart from "./Components/Charts/Doughnut";
-// import PatientAnomalyDashboard from "./Components/Screens/PatientAnomalyDashboard";
-import ProvidersAnomalyDashboard from "./Components/Screens/ProvidersAnomalyDashboard";
+import PatientAnomalyDashboard from "./Components/Screens/PatientAnomalyDashboard";
+// import ProvidersAnomalyDashboard from "./Components/Screens/ProvidersAnomalyDashboard";
 import "./styles.css";
 
 export default function App() {
@@ -14,8 +14,8 @@ export default function App() {
       <LineCust />
       <Linestacked />
       <DChart /> */}
-      {/* <PatientAnomalyDashboard /> */}
-      <ProvidersAnomalyDashboard />
+      <PatientAnomalyDashboard />
+      {/* <ProvidersAnomalyDashboard /> */}
     </div>
   );
 }
--- src/Components/Screens/PatientAnomalyDashboard.jsx
+++ src/Components/Screens/PatientAnomalyDashboard.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { Grid, Typography, Card, CardContent } from "@material-ui/core";
-import Select from "react-select";
+// import Select from "react-select";
 import LineCust from "../Charts/LineCust";
 import Linestacked from "../Charts/Linestacked";
+import ChartCard from "../Cards/ChartCard";
 
 export default class doctorsChart extends Component {
   // constructor(props) {
@@ -14,36 +15,29 @@ export default class doctorsChart extends Component {
       <div style={{ padding: "15px" }}>
         <Grid container spacing={4} justify="space-between">
           <Grid item xs={12}>
-            <Card>
-              <CardContent>
-                <LineCust />
-              </CardContent>
-            </Card>
+            <ChartCard title="Patient" chart={<LineCust />} />
           </Grid>
         </Grid>
         <Grid container spacing={4} justify="space-between">
           <Grid item xs={4}>
-            <Card>
-              <CardContent>
-                <Linestacked />
-              </CardContent>
-            </Card>
+            <ChartCard
+              title="Symptom to Doctor Anomaly"
+              chart={<Linestacked />}
+            />
           </Grid>
 
           <Grid item xs={4}>
-            <Card>
-              <CardContent>
-                <Linestacked />
-              </CardContent>
-            </Card>
+            <ChartCard
+              title="Symptom to Test Anomaly"
+              chart={<Linestacked />}
+            />
           </Grid>
 
           <Grid item xs={4}>
-            <Card>
-              <CardContent>
-                <Linestacked />
-              </CardContent>
-            </Card>
+            <ChartCard
+              title="Symptom to Medication Anomaly"
+              chart={<Linestacked />}
+            />
           </Grid>
         </Grid>
       </div>
