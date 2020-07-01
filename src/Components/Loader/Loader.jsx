10380de3b49bd28de3a1ccafe159d1423083a865
SikandarZameer <46181276+SikandarZameer@users.noreply.github.com>
2020-07-02 00:19:43 +0500

    Loader-added

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loader.css";
+
+export default () => {
+  return (
+    <div id="loader">
+      <div className="ball" />
+      <div className="ball" />
+      <div className="ball" />
+    </div>
+  );
+};
